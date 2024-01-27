2fe63b36d665fc8714afc22b1fd32c2365bde373
Emmanuel Andem <emmanuelandem360@gmail.com>
2024-01-27 16:11:35 +0100

    style: update MobileSidebar spacing

--- src/components/layout/MobileSidebar.tsx
+++ src/components/layout/MobileSidebar.tsx
@@ -47,13 +47,13 @@ const MobileSidebar = ({ expanded }: MobileSidebarProps) => {
     <div
       className={`${
         expanded ? "scale-x-100" : "scale-x-0"
-      } origin-left transition-all duration-300 ease-out pt-16 fixed top-0 left-0 min-h-screen w-[40vw] flex flex-col justify-between gap-8 bg-veryLightGray border-r border-[#EBECF2] dark:bg-darkGreen dark:border-paleGreen z-30`}
+      } origin-left transition-all duration-300 ease-out pt-16 fixed top-0 left-0 min-h-screen w-[40vw] flex flex-col lg:justify-between gap-4 lg:gap-8 bg-veryLightGray border-r border-[#EBECF2] dark:bg-darkGreen dark:border-paleGreen z-30`}
     >
       <div>
         <div className="my-5 flex items-center justify-center">
           <img src={logo} />
         </div>
-        <nav className="flex flex-col gap-4 ">
+        <nav className="flex flex-col gap-2 lg:gap-4 ">
           {LINKS.map(({ icon, to }) => (
             <SidebarIcon icon={icon} to={to} />
           ))}
@@ -62,7 +62,7 @@ const MobileSidebar = ({ expanded }: MobileSidebarProps) => {
           <ThemeToggle />
         </div>
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2 lg:gap-4">
         <SidebarIcon icon={arrowRight} />
         <SidebarIcon icon={settingsIcon} />
         <SidebarIcon icon={logoutIcon} />
