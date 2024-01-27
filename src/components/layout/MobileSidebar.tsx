e9edafa0f809a6b92ecddbdf48a896abc273836a
Emmanuel Andem <emmanuelandem360@gmail.com>
2024-01-27 16:05:44 +0100

    style: update MobileSidebar

--- src/components/layout/MobileSidebar.tsx
+++ src/components/layout/MobileSidebar.tsx
@@ -47,7 +47,7 @@ const MobileSidebar = ({ expanded }: MobileSidebarProps) => {
     <div
       className={`${
         expanded ? "scale-x-100" : "scale-x-0"
-      } origin-left transition-all duration-300 ease-out pt-16 absolute top-0 left-0 bottom-0 w-[40vw] flex flex-col justify-between gap-8 bg-veryLightGray border-r border-[#EBECF2] dark:bg-darkGreen dark:border-paleGreen z-30`}
+      } origin-left transition-all duration-300 ease-out pt-16 fixed top-0 left-0 min-h-screen w-[40vw] flex flex-col justify-between gap-8 bg-veryLightGray border-r border-[#EBECF2] dark:bg-darkGreen dark:border-paleGreen z-30`}
     >
       <div>
         <div className="my-5 flex items-center justify-center">
